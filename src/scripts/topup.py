e2a3d487fa3e916be595398ec5f78b43f21a15b1
Gilles86 <Gilles.de.Hollander@gmail.com>
2017-08-31 18:12:59 +0200

    Rewrote topup pipeline, using DerivativesDataSink

--- src/scripts/topup.py
+++ src/scripts/topup.py
@@ -1,29 +1,64 @@
 import nipype.pipeline.engine as pe
 import nipype.interfaces.io as nio
-from spynoza.io import BIDSGrabber
+from spynoza.io.bids import BIDSGrabber, collect_data, DerivativesDataSink
 from spynoza.unwarping.topup.workflows import create_bids_topup_workflow
+from IPython import embed
+from nipype.utils.filemanip import split_filename
+from operator import itemgetter
 
-dg = pe.Node(BIDSGrabber(), name='datagrabber')
-dg.inputs.subject = '012'
+import nibabel as nb
 
-dg.iterables = [('task', ['binoculardots055', 'binoculardots070']),
-                ('run', [1,2,3,4])]
+subject = '012'
+subject_data, layout = collect_data('/data/sourcedata', subject, 'highres')
 
-dg.iterables = [('task', ['binoculardots055', 'binoculardots070'][:1]),
-                ('run', [1,2,3,4][:1])]
+grabber = pe.Node(BIDSGrabber(anat_only=False), name='bids_grabber')
+grabber.inputs.subject_id = '012'
+grabber.inputs.subject_data =subject_data
 
-workflow = create_bids_topup_workflow(mode='average')
+workflow = pe.Workflow(name='topup')
+workflow.base_dir = '/data/workflow_folders'
 
-for var in ['bold', 'bold_metadata', 'fieldmap', 'fieldmap_metadata']:
-    workflow.connect(dg, var, workflow.get_node('inputnode'), var)
+subworkflows = []
 
-ds = pe.Node(nio.DataSink(base_directory='/data/tests'),
-             name='datasink')
+def get_item(l, idx):
+    return l[idx]
 
-workflow.connect(workflow.get_node('outputnode'), 'out_field', ds, 'out_field')
-workflow.connect(workflow.get_node('outputnode'), 'out_warp', ds, 'out_warp')
-workflow.connect(workflow.get_node('outputnode'), 'unwarped_image', ds, 'unwarped_image')
-workflow.connect(workflow.get_node('outputnode'), 'mean_bold', ds, 'mean_bold')
+for i, bold_file in enumerate(subject_data['bold']):
 
-workflow.run(plugin='MultiProc', plugin_args={'n_procs' : 3})
+    # Get different names for indivdiual workflows, based on sid/run/task
+    fname = split_filename(bold_file)[1]
+    fname_nosub = '_'.join(fname.split("_")[1:])
+    name = "func_topup_" + fname_nosub.replace(
+        ".", "_").replace(" ", "").replace("-", "_").replace("_bold", "_wf")
 
+    # Create sub-topup-workflow
+    subworkflows.append(create_bids_topup_workflow(mode='average', name=name))
+    workflow.connect(grabber, ('bold', get_item, i), subworkflows[i], 'inputnode.bold')
+
+    # Find fieldmap and metadata
+    fieldmap = layout.get_fieldmap(bold_file)['epi']
+    subworkflows[i].inputs.inputnode.fieldmap = fieldmap
+    bold_metadata = layout.get_metadata(bold_file)
+    bold_metadata['NDynamics'] = nb.load(bold_file).shape[-1]
+
+    fieldmap_metadata = layout.get_metadata(fieldmap)
+    fieldmap_metadata['NDynamics'] = nb.load(fieldmap).shape[-1]
+
+    subworkflows[i].inputs.inputnode.bold_metadata = bold_metadata
+    subworkflows[i].inputs.inputnode.fieldmap_metadata = fieldmap_metadata
+
+    ds_warpfield = pe.Node(DerivativesDataSink(base_directory='/data/derivatives/topup',
+                                              source_file=bold_file,
+                                              suffix='warpfield'),
+                              name='ds_warpfield')
+    subworkflows[i].connect(subworkflows[i].get_node('outputnode'), 'out_warp', ds_warpfield,'in_file')
+    
+    
+    ds_unwarped = pe.Node(DerivativesDataSink(base_directory='/data/derivatives/topup',
+                                              source_file=bold_file,
+                                              suffix='unwarped'),
+                              name='ds_unwarped')
+    subworkflows[i].connect(subworkflows[i].get_node('outputnode'), 'unwarped_image', ds_unwarped,'in_file')
+
+
+workflow.run(plugin='MultiProc', plugin_args={'n_procs' : 8})
