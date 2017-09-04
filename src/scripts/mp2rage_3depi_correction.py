2a4273fe4be317bd230e787101fc1c13b6473740
Gilles de Hollander <gilles.de.hollander@gmail.com>
2017-09-04 16:51:28 +0200

    Script to apply new mp2rage_3d_epi_correction to 7T hires data

--- src/scripts/mp2rage_3depi_correction.py
+++ src/scripts/mp2rage_3depi_correction.py
@@ -1,10 +1,16 @@
 from spynoza.unwarping.mp2rage_3depi.workflows import create_3DEPI_registration_workflow
 
-wf = create_3DEPI_registration_workflow(init_reg_file='/data/derivatives/manual_transforms/sub-012_from-3DEPI_to-highres.lta')
+
+init_reg_file = '/data/derivatives/manual_transforms/sub-012_from-3DEPI_to-highres_ras.lta'
+
+wf = create_3DEPI_registration_workflow(init_reg_file=init_reg_file)
 wf.base_dir = '/tmp'
 
-wf.inputs.inputnode.INV2 = '/data/sourcedata/sub-012/anat/sub-012_acq-3DEPI_INV2.nii'
-wf.inputs.inputnode.T1_EPI_file = '/data/sourcedata/sub-012/anat/sub-012_acq-3DEPI_T1w.nii'
-wf.inputs.inputnode.T1_file = '/data/derivatives/preproc_anat/fmriprep/sub-012/anat/sub-012_acq-highres_T1w_preproc.nii.gz'
+wf.inputs.inputnode.INV2 = '/data/derivatives/reorient/sub-012/anat/sub-012_acq-3DEPI_INV2_ras.nii.gz'
+wf.inputs.inputnode.T1w_EPI_file = '/data/derivatives/reorient/sub-012/anat/sub-012_acq-3DEPI_T1w_ras.nii.gz'
+wf.inputs.inputnode.T1w_file = '/data/derivatives/preproc_anat/fmriprep/sub-012/anat/sub-012_acq-highres_T1w_preproc.nii.gz'
+wf.inputs.inputnode.bold_EPIs = ['/data/sourcedata/sub-012/func/sub-012_task-binoculardots055_run-1_bold.nii.gz',
+                                 '/data/sourcedata/sub-012/func/sub-012_task-binoculardots055_run-2_bold.nii.gz',
+                                 '/data/sourcedata/sub-012/func/sub-012_task-binoculardots055_run-3_bold.nii.gz']
 
 wf.run()
