a5abc5fb61a5d00f250c9a1b309b12419a821d11
Gilles de Hollander <gilles.de.hollander@gmail.com>
2017-09-05 11:25:44 +0200

    Try registration with ANTS (Still suboptimal results)

--- src/scripts/epi2t1.py
+++ src/scripts/epi2t1.py
@@ -3,37 +3,44 @@ import nipype.interfaces.io as nio
 import nipype.interfaces.utility as niu
 import nipype.pipeline.engine as pe
 import nipype.interfaces.io as nio
+from spynoza.io.bids import DerivativesDataSink
 
 import os
-os.environ['SUBJECTS_DIR'] = '/data/freesurfer'
-
 identity = pe.Node(niu.IdentityInterface(fields=['subject']), name='identity')
 identity.iterables = [('subject', ['012'])]
 
-templates = {'EPI_space_file':'/data/tests/unwarped_image/_run_{run}_task_{task}/sub-{subject}_task-{task}_run-{run}_bold_masked_mcf_mean_trans.nii.gz',
-             'T1':'/data/freesurfer/sub-{subject}/mri/T1.nii.gz'}
+templates = {'EPI_space_file':'/data/derivatives/topup/sub-{subject}/func/sub-{subject}_task-{task}_run-{run}_bold_unwarped.nii.gz',
+             'T1':'/data/derivatives/preproc_anat/fmriprep/sub-{subject}/anat/sub-{subject}_acq-highres_T1w_preproc.nii.gz',
+             'manual_transform':'/data/derivatives/manual_transforms/sub-{subject}_task-{task}_run-{run}_to_T1w_preproc.lta'}
 
 selector = pe.Node(nio.SelectFiles(templates), name='selector')
-selector.iterables = [('run', [1,2,3,4]), ('task', ['binoculardots055', 'binoculardots070'])]
+selector.iterables = [('run', [1,2,3,4, 5]), ('task', ['binoculardots055', 'binoculardots070'][:2])]
 
-init_reg_file = '/data/freesurfer/sub-012/sub-012_task-binoculardots070_run-1_to_T1.lta'
-wf = create_epi_to_T1_workflow(init_reg_file=init_reg_file, use_FS=True)
+init_reg_file = '/data/derivatives/manual_transforms/sub-012_task-binoculardots055_run-1_to_T1w_preproc.lta'
+wf = create_epi_to_T1_workflow(init_reg_file=init_reg_file, package='ants', apply_transform=True)
 wf.base_dir = '/tmp/'
-
-def make_fs_subject_id(subject):
-    return 'sub-%s' % subject
 
 wf.connect(identity, 'subject', selector, 'subject')
 wf.connect(selector, 'EPI_space_file', wf.get_node('inputspec'), 'EPI_space_file')
 wf.connect(selector, 'T1', wf.get_node('inputspec'), 'T1_file')
-wf.connect(identity, ('subject', make_fs_subject_id), wf.get_node('inputspec'), 'freesurfer_subject_ID')
+wf.connect(selector, 'manual_transform', wf.get_node('convert_to_itk'), 'in_lta')
 
-wf.inputs.inputspec.freesurfer_subject_dir = '/data/freesurfer'
+ds_epi2t1_mat = pe.Node(DerivativesDataSink(base_directory='/data/derivatives/epi_to_T1',
+                                          suffix='epi2t1'),
+                      name='ds_epi2t1_mat')
+wf.connect(wf.get_node('outputspec'), 'EPI_T1_matrix_file', ds_epi2t1_mat, 'in_file')
+wf.connect(selector, 'EPI_space_file', ds_epi2t1_mat, 'source_file')
 
-ds = pe.Node(nio.DataSink(base_directory='/data/derivatives'), 'datasink')
+ds_epi2t1_warped = pe.Node(DerivativesDataSink(base_directory='/data/derivatives/epi_to_T1',
+                                          suffix='epi2t1'),
+                      name='ds_epi2t1_warped')
+wf.connect(wf.get_node('outputspec'), 'transformed_EPI_space_file', ds_epi2t1_warped, 'in_file')
+wf.connect(selector, 'EPI_space_file', ds_epi2t1_warped, 'source_file')
+
+ds = pe.Node(nio.DataSink(base_directory='/data/derivatives/epi_to_T1'), 'datasink')
 
 wf.connect(wf.get_node('outputspec'), 'EPI_T1_register_file', ds, 'EPI_T1_register_file')
 wf.connect(wf.get_node('outputspec'), 'EPI_T1_matrix_file', ds, 'EPI_T1_matrix_file')
 wf.connect(wf.get_node('outputspec'), 'T1_EPI_matrix_file', ds, 'T1_EPI_matrix_file')
-
+wf.connect(wf.get_node('outputspec'), 'transformed_EPI_space_file', ds, 'warped_EPI_file')
 wf.run()
