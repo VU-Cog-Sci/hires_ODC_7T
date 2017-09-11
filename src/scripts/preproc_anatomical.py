63d4dde7906aeff663cf99b173161f400755f6af
Gilles86 <Gilles.de.Hollander@gmail.com>
2017-09-11 11:44:10 +0200

    Skull strip using ANTS

--- src/scripts/preproc_anatomical.py
+++ src/scripts/preproc_anatomical.py
@@ -1,19 +1,28 @@
 from fmriprep.workflows import base
 import os
+from niworkflows.nipype.pipeline import engine as pe
+from fmriprep.interfaces import BIDSFreeSurferDir
+
 
 
 os.environ['SUBJECTS_DIR'] = '/data/freesurfer'
 
-skull_strip_ants = False
+skull_strip_ants = True
 output_spaces = ['T1w', 'template', 'fsnative']
 template = 'MNI152NLin2009cAsym'
 debug = False
-freesurfer = True
+freesurfer = False
 longitudinal = False
-omp_nthreads = 4
+omp_nthreads = 10
 hires = True
 reportlets_dir = '/data/reportlets/anat'
-output_dir = '/data/derivative/anat'
+output_dir = '/data/derivatives/preproc_anat'
+output_spaces = ['T1w', 'template']
+
+fsdir = pe.Node(BIDSFreeSurferDir(derivatives=output_dir,
+                                  freesurfer_home=os.getenv('FREESURFER_HOME'),
+                                  spaces=output_spaces),
+                        name='fsdir', run_without_submitting=True)
 
 wf = base.init_single_subject_wf(subject_id='012',
                                  task_id='binoculardots055',
@@ -22,12 +31,12 @@ wf = base.init_single_subject_wf(subject_id='012',
                                  debug=False,
                                  anat_only=True,
                                  longitudinal=False,
-                                 skull_strip_ants=False,
+                                 skull_strip_ants=skull_strip_ants,
                                  reportlets_dir='/data/reportlets/anat',
-                                 output_dir='/data/derivative/anat',
+                                 output_dir=output_dir,
                                  bids_dir='/data/sourcedata/',
-                                 freesurfer=True,
-                                 output_spaces=['T1w', 'template', 'fsnative'],
+                                 freesurfer=freesurfer,
+                                 output_spaces=output_spaces,
                                  hires=True,
                                  bold2t1w_dof=12,
                                  fmap_bspline=False,
@@ -40,15 +49,17 @@ wf = base.init_single_subject_wf(subject_id='012',
                                  omp_nthreads=4,
                                  template='MNI152NLin2009cAsym')
 
+wf.connect(fsdir, 'subjects_dir', wf.get_node('inputnode'), 'subjects_dir')
+
 
 wf.base_dir = '/data/workflow_folders/'
 
 plugin_settings = {}
 plugin_settings['plugin'] = 'MultiProc'
-plugin_settings['plugin_args'] = {'n_procs': 3}
+plugin_settings['plugin_args'] = {'n_procs': 10}
 
 plugin_settings['plugin'] = 'Linear'
 
 
-#wf.write_graph()
+wf.write_graph()
 wf.run(**plugin_settings)
